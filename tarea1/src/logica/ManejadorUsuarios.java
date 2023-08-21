e44ea106146688ce49a4c81e67f45191e4ddef5a
Lautaro <138635358+Zeeka24@users.noreply.github.com>
2023-08-21 03:55:45 -0300

    implementados altaEmpresa(), AltaPostulante(), obtenerUsuario()  y obtenerListaUsuarios() en ManejadorUsuarios

--- tarea1/src/logica/ManejadorUsuarios.java
+++ tarea1/src/logica/ManejadorUsuarios.java
@@ -1,10 +1,13 @@
 package logica;
 
-import java.util.TreeMap;
+import java.util.*;
+
+import excepciones.NicknameNoExisteException;
+import utils.DTUsuario;
 
 public class ManejadorUsuarios {
 
-	private TreeMap<String, Usuario> coleccionUsuarios;
+	private Map<String, Usuario> coleccionUsuarios = new TreeMap<>();
 	private static ManejadorUsuarios instancia;
 	
 	public static ManejadorUsuarios getInstance() {
@@ -13,5 +16,52 @@ public class ManejadorUsuarios {
         }
         return instancia;
     }
+	
+	/**
+	 * Crea la empresa y la agrega a coleccionUsuarios.
+	 */
+	void altaEmpresa(String nickname, String nombre, String apellido, String email, String nombreEmpresa, String descripcion, String linkWeb) {
+		coleccionUsuarios.put(nickname.toLowerCase(), new Empresa(nickname, nombre, apellido, email, nombreEmpresa, descripcion, linkWeb));
+	}
+	
+	/**
+	 * Crea el postulante y lo agrega a coleccionUsuarios.
+	 */
+	void altaPostulante(String nickname, String nombre, String apellido, String email, String fechaNacimiento, String nacionalidad) {
+		coleccionUsuarios.put(nickname.toLowerCase(), new Postulante(nickname, nombre, apellido, email, fechaNacimiento, nacionalidad));
+	}
+	
+	/**
+	 * Devuelve un DTUsuario con los datos del usuario con el nickname dado.
+	 * Si el nickname no existe en el sistema tira un NicknameNoExisteException.
+	 * Si el nickname es NULL tira una unchecked exception.
+	 */
+	DTUsuario obtenerUsuario(String nickname) throws NicknameNoExisteException {
+		String nicknameLowerCase = nickname.toLowerCase();
+		if ( coleccionUsuarios.containsKey(nicknameLowerCase) ) {
+			return coleccionUsuarios.get(nicknameLowerCase).toDataType();
+		} else {
+			throw new NicknameNoExisteException("El usuario con el nickname " + nickname + " no existe.");
+		}
+		
+	}
+	
+	/**
+	 * Devuelve una lista de DTUsuario con la informacion de todos los usuarios registrados en el sistema.
+	 */
+	List<DTUsuario> obtenerListaUsuarios() {
+		List<DTUsuario> out = new ArrayList<DTUsuario>();
+		for (Map.Entry<String, Usuario> entry : coleccionUsuarios.entrySet()) {
+			out.add(entry.getValue().toDataType());
+		}
+		return out;
+		/*
+		 * Otra forma de implementarlo: 
+		return coleccionUsuarios.entrySet()
+		.stream()
+		.map(entry -> entry.getValue().toDataType())
+		.collect(Collectors.toList());
+		*/
+	}
 
 }
