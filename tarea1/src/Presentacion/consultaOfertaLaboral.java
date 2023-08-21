8b2d9d50a4ebf5326e816a36b16a0f08cd4db450
joaccocaballero <joaquincaballero182@gmail.com>
2023-08-21 15:31:00 -0300

    avances swing

--- tarea1/src/Presentacion/consultaOfertaLaboral.java
+++ tarea1/src/Presentacion/consultaOfertaLaboral.java
@@ -7,8 +7,20 @@ import java.awt.FlowLayout;
 import javax.swing.JPanel;
 import java.awt.Panel;
 import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JSeparator;
+import java.awt.ScrollPane;
+import java.awt.Scrollbar;
+import javax.swing.JTable;
+import java.awt.Font;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import utilsPresentacion.CentrarColumnas;
 
 public class consultaOfertaLaboral extends JInternalFrame {
+	private JTable tablaOfertaLaboral;
 
 	/**
 	 * Launch the application.
@@ -30,13 +42,65 @@ public class consultaOfertaLaboral extends JInternalFrame {
 	 * Create the frame.
 	 */
 	public consultaOfertaLaboral() {
+		setResizable(true);
 		setMaximizable(true);
 		setIconifiable(true);
 		setClosable(true);
 		setTitle("Consulta de Oferta Laboral");
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		setBounds(100, 100, 1027, 687);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Seleccione una empresa:");
+		lblNewLabel.setBounds(21, 22, 736, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(21, 38, 483, 22);
+		getContentPane().add(comboBox);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(21, 71, 959, 2);
+		getContentPane().add(separator);
+		
+		JLabel lblNewLabel_1 = new JLabel("Ofertas Laborales Asociadas:");
+		lblNewLabel_1.setBounds(21, 87, 450, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		ScrollPane panelOfertas = new ScrollPane();
+		panelOfertas.setBounds(21, 107, 933, 540);
+		getContentPane().add(panelOfertas);
+		
+		Scrollbar scrollbar = new Scrollbar();
+		scrollbar.setBounds(937, 107, 17, 540);
+		panelOfertas.add(scrollbar);
+		
+		tablaOfertaLaboral = new JTable();
+		tablaOfertaLaboral.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo"},
+				{"Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo", "Ejemplo"},
+			},
+			new String[] {
+				"Nombre", "Descripci\u00F3n", "Ciudad", "Departamento", "Horario", "Remuneraci\u00F3n", "Fecha de Alta"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class, String.class, String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		tablaOfertaLaboral.setDefaultRenderer(Object.class, new CentrarColumnas());
+		JTableHeader headerOfertas = tablaOfertaLaboral.getTableHeader();
+		tablaOfertaLaboral.getColumnModel().getColumn(0).setPreferredWidth(72);
+		tablaOfertaLaboral.getColumnModel().getColumn(3).setPreferredWidth(100);
+		tablaOfertaLaboral.getColumnModel().getColumn(5).setPreferredWidth(120);
+		tablaOfertaLaboral.getColumnModel().getColumn(6).setPreferredWidth(105);
+		tablaOfertaLaboral.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		tablaOfertaLaboral.setBounds(197, 259, 1, 1);
+		panelOfertas.add(headerOfertas);
+		panelOfertas.add(tablaOfertaLaboral);
 
 	}
-
 }
--- tarea1/src/Presentacion/consultaUsuario.java
+++ tarea1/src/Presentacion/consultaUsuario.java
@@ -12,13 +12,21 @@ import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JSeparator;
 import javax.swing.JEditorPane;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+
+import utilsPresentacion.CentrarColumnas;
+
 import java.awt.ScrollPane;
 import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
 import javax.swing.JScrollBar;
 
 public class consultaUsuario extends JInternalFrame {
-	private JTable table;
+	private JTable tablaUsuario;
+	private JTable tablaPostulante;
+	private JTable tablaEmpresa;
 
 	/**
 	 * Launch the application.
@@ -45,25 +53,31 @@ public class consultaUsuario extends JInternalFrame {
 		setIconifiable(true);
 		setClosable(true);
 		setTitle("Consulta de Usuario");
-		setBounds(100, 100, 840, 565);
+		setBounds(100, 100, 1065, 784);
 		getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Seleccione un Usuario:");
-		lblNewLabel.setBounds(10, 11, 804, 14);
+		JLabel lblNewLabel = new JLabel("Seleccione un usuario:");
+		lblNewLabel.setBounds(20, 11, 931, 14);
 		getContentPane().add(lblNewLabel);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(10, 36, 427, 22);
+		comboBox.setBounds(20, 36, 544, 22);
 		getContentPane().add(comboBox);
 		
-		ScrollPane scrollPane = new ScrollPane();
-		scrollPane.setBounds(10, 69, 718, 294);
-		getContentPane().add(scrollPane);
+		JSeparator separator = new JSeparator();
+		separator.setBounds(20, 67, 1019, 14);
+		getContentPane().add(separator);
 		
-		table = new JTable();
-		table.setModel(new DefaultTableModel(
+		Panel panelUsuario = new Panel();
+		panelUsuario.setBounds(20, 83, 585, 70);
+		getContentPane().add(panelUsuario);
+		
+		tablaUsuario = new JTable();
+		tablaUsuario.setFillsViewportHeight(true);
+
+		tablaUsuario.setModel(new DefaultTableModel(
 			new Object[][] {
-				{null, null, null, null},
+				{"Juan52", "Juan", "Rodríguez", "jr@fing.edu.uy"},
 			},
 			new String[] {
 				"Nickname", "Nombre", "Apellido", "Correo"
@@ -76,8 +90,56 @@ public class consultaUsuario extends JInternalFrame {
 				return columnTypes[columnIndex];
 			}
 		});
-		table.setBounds(350, 189, 1, 1);
-		scrollPane.add(table);
+		tablaUsuario.setDefaultRenderer(Object.class, new CentrarColumnas());
+		JTableHeader headerUsuario = tablaUsuario.getTableHeader();
+		tablaUsuario.getColumnModel().getColumn(0).setPreferredWidth(110);
+		tablaUsuario.getColumnModel().getColumn(1).setPreferredWidth(114);
+		tablaUsuario.getColumnModel().getColumn(2).setPreferredWidth(109);
+		tablaUsuario.getColumnModel().getColumn(3).setPreferredWidth(251);
+		panelUsuario.add(headerUsuario);
+		panelUsuario.add(tablaUsuario);
+		
+		Panel panelPostulante = new Panel();
+		panelPostulante.setBounds(20, 157, 370, 70);
+		getContentPane().add(panelPostulante);
+		
+		tablaPostulante = new JTable();
+		tablaPostulante.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Postulante", "25/03/1991", "Uruguayo"},
+			},
+			new String[] {
+				"Tipo de Usuario", "Fecha de Nacimiento", "Nacionalidad"
+			}
+		));
+		tablaPostulante.setDefaultRenderer(Object.class, new CentrarColumnas());
+		JTableHeader headerPostulante = tablaPostulante.getTableHeader();
+		tablaPostulante.getColumnModel().getColumn(0).setPreferredWidth(109);
+		tablaPostulante.getColumnModel().getColumn(1).setPreferredWidth(127);
+		tablaPostulante.getColumnModel().getColumn(2).setPreferredWidth(124);
+		panelPostulante.add(headerPostulante);
+		panelPostulante.add(tablaPostulante);
+		
+		Panel panelEmpresa = new Panel();
+		panelEmpresa.setBounds(20, 233, 585, 59);
+		getContentPane().add(panelEmpresa);
+		
+		tablaEmpresa = new JTable();
+		tablaEmpresa.setModel(new DefaultTableModel(
+			new Object[][] {
+				{"Empresa", "Empresa", "Empresa"},
+			},
+			new String[] {
+				"Nombre de Empresa", "Descripci\u00F3n", "Link Web"
+			}
+		));
+		tablaEmpresa.setDefaultRenderer(Object.class, new CentrarColumnas());
+		JTableHeader headerEmpresa = tablaEmpresa.getTableHeader();
+		tablaEmpresa.getColumnModel().getColumn(0).setPreferredWidth(143);
+		tablaEmpresa.getColumnModel().getColumn(1).setPreferredWidth(240);
+		tablaEmpresa.getColumnModel().getColumn(2).setPreferredWidth(191);
+		panelEmpresa.add(headerEmpresa);
+		panelEmpresa.add(tablaEmpresa);
 
 	}
 }
