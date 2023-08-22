2e40c63611969157a99cf0ef767aba2fabf64816
Lautaro <138635358+Zeeka24@users.noreply.github.com>
2023-08-22 17:22:08 -0300

    comentada la funcion obtenerUsuario en ManejadorUsuarios porque falta arreglarla

--- tarea1/src/logica/ManejadorUsuarios.java
+++ tarea1/src/logica/ManejadorUsuarios.java
@@ -1,13 +1,12 @@
 package logica;
 
 import java.util.*;
-
-import excepciones.NicknameNoExisteException;
+import excepciones.*;
 import utils.DTUsuario;
 
 public class ManejadorUsuarios {
 
-	private Map<String, Usuario> coleccionUsuarios = new TreeMap<>();
+	private Map<String, Usuario> coleccionUsuarios = new HashMap<String,Usuario>();
 	private static ManejadorUsuarios instancia;
 	
 	public static ManejadorUsuarios getInstance() {
@@ -30,12 +29,14 @@ public class ManejadorUsuarios {
 	void altaPostulante(String nickname, String nombre, String apellido, String email, String fechaNacimiento, String nacionalidad) {
 		coleccionUsuarios.put(nickname.toLowerCase(), new Postulante(nickname, nombre, apellido, email, fechaNacimiento, nacionalidad));
 	}
-	
+
+	// TODO: el DT que devuelve tiene que contener las ofertasAsociadas en caso de ser una empresa o las postulacionesAsociadas en caso de ser un postulante.
 	/**
 	 * Devuelve un DTUsuario con los datos del usuario con el nickname dado.
 	 * Si el nickname no existe en el sistema tira un NicknameNoExisteException.
 	 * Si el nickname es NULL tira una unchecked exception.
 	 */
+	/*
 	DTUsuario obtenerUsuario(String nickname) throws NicknameNoExisteException {
 		String nicknameLowerCase = nickname.toLowerCase();
 		if ( coleccionUsuarios.containsKey(nicknameLowerCase) ) {
@@ -45,23 +46,28 @@ public class ManejadorUsuarios {
 		}
 		
 	}
+	*/
 	
 	/**
-	 * Devuelve una lista de DTUsuario con la informacion de todos los usuarios registrados en el sistema.
+	 * Devuelve una lista de DTUsuario con la informacion de todos los usuarios registrados en el sistema ordenados segun su cedula.
+	 * Si no hay usuarios registrados devuelve una lista vacia.
 	 */
 	List<DTUsuario> obtenerListaUsuarios() {
 		List<DTUsuario> out = new ArrayList<DTUsuario>();
 		for (Map.Entry<String, Usuario> entry : coleccionUsuarios.entrySet()) {
 			out.add(entry.getValue().toDataType());
 		}
+		Collections.sort(out, new Comparator<DTUsuario>() {
+			@Override
+			public int compare(DTUsuario usuario1, DTUsuario usuario2) {
+				int comparacionNombre = usuario1.getNombre().compareTo(usuario2.getNombre());
+				if (comparacionNombre == 0) {
+					return usuario1.getApellido().compareTo(usuario2.getApellido());
+				}
+				return comparacionNombre;
+			}
+		});
 		return out;
-		/*
-		 * Otra forma de implementarlo: 
-		return coleccionUsuarios.entrySet()
-		.stream()
-		.map(entry -> entry.getValue().toDataType())
-		.collect(Collectors.toList());
-		*/
 	}
 
 }
