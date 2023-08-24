cd2638715dd6e595ef3b0bbdb297ae0aa43d133a
Philippe Bedrossian <philippebedross@gmail.com>
2023-08-24 19:09:33 -0300

    pre merge

--- tarea1/src/logica/ManejadorOfertaLaboral.java
+++ tarea1/src/logica/ManejadorOfertaLaboral.java
@@ -1,10 +1,14 @@
 package logica;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
+
+import excepciones.NombreExisteException;
 
 public class ManejadorOfertaLaboral {
 
-	private TreeMap<String, OfertaLaboral> coleccionOfertasLaborales;
+	private Map<String, OfertaLaboral> coleccionOfertasLaborales = new HashMap<String, OfertaLaboral>();
 	private static ManejadorOfertaLaboral instancia;
 	
 	public static ManejadorOfertaLaboral getInstance() {
@@ -13,4 +17,11 @@ public class ManejadorOfertaLaboral {
         }
         return instancia;
     }
+	
+	public void addOferta(OfertaLaboral ofL) throws NombreExisteException{
+		if (coleccionOfertasLaborales.containsKey(ofL.getNombre())) {
+			throw new NombreExisteException("Ya existe una oferta laboral con el nombre ingresado");
+		}
+		coleccionOfertasLaborales.put(ofL.getNombre(), ofL);
+	}
 }
