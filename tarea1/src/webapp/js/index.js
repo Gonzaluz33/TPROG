270e8489d57fcebeb4cfc92c6a34fd5a6326c784
joaccocaballero <joaquincaballero182@gmail.com>
2023-09-11 15:20:05 -0300

    index.html visitante

--- tarea1/src/webapp/js/index.js
+++ tarea1/src/webapp/js/index.js
@@ -1,7 +1,18 @@
 const keywords = ["Tiempo Completo", "Medio Tiempo", "Remoto", "Freelance", "Temporal", "Permanente", "Computación", "Administración", "Logística", "Contabilidad"];
 const mainContentData = [
-    { image: 'src/img/example_1.png', text: 'Contenido de ejemplo' },
-    { image: 'src/img/example_2.png', text: 'Contenido de ejemplo' }
+    {
+        image: 'src/img/example_1.png', nombreOferta: 'Desarrollador Frontend', descripcion: ' Unete a nuestro equipo de desarrollo frontend y crea experiencias de usuario excepcionales.',
+        departamento: 'Montevideo', ciudad: 'Montevideo',
+        keywords: ["Tiempo Completo", "Medio Tiempo" ,"Remoto", "Freelance", "Temporal", "Permanente"]
+    },
+    { 
+        image: 'src/img/example_2.png', 
+        nombreOferta: 'Estratega de Negocios ', 
+        descripcion: 'Forma parte de nuestro equipo de estrategia y contribuye al crecimiento de las empresas clientes.',
+        departamento: 'Maldonado', 
+        ciudad: 'Punta del Este',
+        keywords: ["Temporal"]
+    }
     // ... más datos
 ];
 
@@ -31,26 +42,40 @@ function cargarKeywords(keywords){
         $("#keywordList").append(ulElement);
         liElement.classList.add("p-2");
     });
-
 }
+
 function cargarContenidoPrincipal() {
 mainContentData.forEach(data => {
+    const etiquetasHTML = data.keywords.map(item => `#${item.replace(/\s+/g, '')}` )
+    const keywords = etiquetasHTML.map(etiqueta => `<b>${etiqueta}</b>`).join('');
     const contentDiv = `
-        <div class="d-flex p-3 border border-dark align-items-center mb-2">
-            <div style="width: 33%;">
-                <img class="w-75" src="${data.image}" alt="">
-            </div>
-            <div class="w-75">
-                <p>${data.text}</p>
-            </div>
-        </div>
+       <div class="d-flex p-2 border border-dark align-items-center mb-2">
+                  <div style="width: 25%;">
+                    <img class="w-75" src="${data.image}" alt="">
+                  </div>
+                  <div class="w-75">
+                    <div class="d-flex flex-column">
+                        <h3> ${data.nombreOferta}</h3>
+                        <div>
+                            <p>
+                               ${data.descripcion}
+                            </p>
+                        </div>
+                        <div class="d-flex flex-column mb-2">
+                            <b>Departamento:  ${data.departamento}</b>
+                            <b>Ciudad:  ${data.ciudad}</b>
+                        </div>
+                        <div class="d-flex gap-1 justify-content-start">
+                            ${keywords}
+                        </div>
+                        <div class="d-flex justify-content-end">
+                            <a href="#" class="text-dark">Ver más</a>
+                        </div>
+                    </div>
+                  </div>
+                </div>
     `;
     $('#mainContent').append(contentDiv);
 });
 }
-
-
-
-
-
 
