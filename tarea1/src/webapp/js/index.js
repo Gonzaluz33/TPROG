da312e3f900acbc08f3c604e38c8d936a880d0ce
joaccocaballero <joaquincaballero182@gmail.com>
2023-09-12 15:50:30 -0300

    Consulta oferta laboral y mi usuario postulante listo

--- tarea1/src/webapp/js/index.js
+++ tarea1/src/webapp/js/index.js
@@ -1,7 +1,7 @@
 const keywords = ["Tiempo Completo", "Medio Tiempo", "Remoto", "Freelance", "Temporal", "Permanente", "Computación", "Administración", "Logística", "Contabilidad"];
 const mainContentData = [
     {
-        image: 'src/img/example_1.png', 
+        image: 'https://tinyurl.com/45nsf34m', 
         nombreOferta: 'Desarrollador Frontend', 
         descripcion: ' Unete a nuestro equipo de desarrollo frontend y crea experiencias de usuario excepcionales.',
         departamento: 'Montevideo', 
@@ -9,12 +9,12 @@ const mainContentData = [
         keywords: ["Tiempo Completo", "Medio Tiempo" ,"Remoto", "Freelance", "Temporal", "Permanente"]
     },
     { 
-        image: 'src/img/example_2.png', 
-        nombreOferta: 'Estratega de Negocios ', 
-        descripcion: 'Forma parte de nuestro equipo de estrategia y contribuye al crecimiento de las empresas clientes.',
-        departamento: 'Maldonado', 
-        ciudad: 'Punta del Este',
-        keywords: ["Temporal"]
+        image: 'https://tinyurl.com/4n2vpurk', 
+        nombreOferta: 'Analista de Marketing Digital', 
+        descripcion: 'Únete a nuestro equipo de marketing y trabaja en estrategias digitales innovadoras.',
+        departamento: 'Flores', 
+        ciudad: 'Flores',
+        keywords: []
     }
     // ... más datos
 ];
@@ -73,7 +73,7 @@ mainContentData.forEach(data => {
                             ${keywords}
                         </div>
                         <div class="d-flex justify-content-end">
-                            <a href="ofertaLaboral.html" class="text-dark">Ver más</a>
+                            <a href="consultaOfertaLaboral.html" class="text-dark">Ver más</a>
                         </div>
                     </div>
                   </div>
