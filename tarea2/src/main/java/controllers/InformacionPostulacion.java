51961c6b420dc272ae10c8877656eb3251bbef33
joaccocaballero <joaquincaballero182@gmail.com>
2023-10-04 18:24:46 -0300

    pom.xml

---
+++ tarea2/src/main/java/controllers/InformacionPostulacion.java
@@ -0,0 +1,56 @@
+package controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class InformacionPostulacion
+ */
+@WebServlet("/informacionPostulacion")
+public class InformacionPostulacion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InformacionPostulacion() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	UtilidadesJWT utilidadesJWT = UtilidadesJWT.obtenerInstancia();
+    	String tipoUsuario = utilidadesJWT.obtenerTipoUsuarioPorRequest(req, resp);
+    	switch(tipoUsuario) {
+    		case ("postulante"):
+    			req.getRequestDispatcher("/WEB-INF/postulante/dashboardPostulante.jsp").forward(req, resp);
+    			break;
+    		case ("empresa"):
+    			req.getRequestDispatcher("/WEB-INF/empresa/informacionPostulacion.jsp").forward(req, resp);
+    			break;
+    		default:
+    			req.getRequestDispatcher("/WEB-INF/visitante/inicio.jsp").forward(req, resp);
+    			break;
+    	}   	
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		processRequest(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
