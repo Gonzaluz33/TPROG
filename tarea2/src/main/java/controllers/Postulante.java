fb64566ba91604676923a538639391554792d490
Gonzalo Luz Alvarez <gonzalo.luz@fing.edu.uy>
2023-11-07 18:52:33 -0300

    login mobile y optimizo servlets (visitante y postulante)

--- tarea2/src/main/java/controllers/Postulante.java
+++ tarea2/src/main/java/controllers/Postulante.java
@@ -6,127 +6,102 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import net.java.dev.jaxb.array.StringArray;
 import servidor.publicar.DtPublicacion;
-import servidor.publicar.DtUsuario;
 import servidor.publicar.EnumEstadoOferta;
 import servidor.publicar.KeywordExisteException_Exception;
+import servidor.publicar.ServicioOfertas;
+import servidor.publicar.ServicioOfertasService;
+import servidor.publicar.ServicioUsuarios;
+import servidor.publicar.ServicioUsuariosService;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Servlet implementation class Postulante
- */
 @WebServlet("/postulante")
 public class Postulante extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private static final long serialVersionUID = 1L;
+    private static final String JWT_COOKIE_NAME = "jwt";
+    private ServicioOfertas portOfertas;
+    private ServicioUsuarios portUsuarios;
+
     public Postulante() {
         super();
-        // TODO Auto-generated constructor stub
+        ServicioOfertasService serviceOfertas = new ServicioOfertasService();
+        portOfertas = serviceOfertas.getServicioOfertasPort();
+        ServicioUsuariosService serviceUsuarios = new ServicioUsuariosService();
+        portUsuarios = serviceUsuarios.getServicioUsuariosPort();
     }
-    
-    public static List<DtPublicacion> filtrarPublicacionesConfirmadas(List<DtPublicacion> publicaciones) {
+
+    private List<DtPublicacion> filtrarPublicacionesConfirmadas(List<DtPublicacion> publicaciones) {
         return publicaciones.stream()
-                .filter(publicacion -> EnumEstadoOferta.CONFIRMADA.equals(publicacion.getDtOferta().getEstado()))
+                .filter(p -> EnumEstadoOferta.CONFIRMADA.equals(p.getDtOferta().getEstado()))
                 .collect(Collectors.toList());
     }
 
-    
-    private void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, KeywordExisteException_Exception {
-    	servidor.publicar.ServicioOfertasService serviceOfertas = new servidor.publicar.ServicioOfertasService();
-        servidor.publicar.ServicioOfertas portOfertas = serviceOfertas.getServicioOfertasPort();
-    	servidor.publicar.ServicioUsuariosService serviceUsuarios = new servidor.publicar.ServicioUsuariosService();
-		servidor.publicar.ServicioUsuarios portUsuarios = serviceUsuarios.getServicioUsuariosPort();
-    	
-        StringArray keywords = portOfertas.obtenerKeywords();
-		req.setAttribute("keywords", keywords.getItem());
-		
-	
-		String busqueda = req.getParameter("busqueda");
-		String[] keywordsSeleccionadas = req.getParameterValues("keywords");
-		String keywordsString = "";
-		if (keywordsSeleccionadas != null) {
-		    keywordsString = String.join("/", keywordsSeleccionadas);
-		}
-		List<DtPublicacion> publicaciones = new ArrayList<>();
-
-		if (busqueda != null && !busqueda.isEmpty()) {
-			publicaciones = portOfertas.obtenerPublicacionesPorBusqueda(busqueda).getItem();
-		} else if (keywordsSeleccionadas != null && keywordsSeleccionadas.length > 0) {
-			publicaciones = portOfertas.obtenerPublicacionesPorKeywords(keywordsString).getItem();
-		} else {
-			publicaciones = portOfertas.obtenerPublicaciones().getItem();
-		}
-
-		List<DtPublicacion> pubFiltered = filtrarPublicacionesConfirmadas(publicaciones);
-		req.setAttribute("publicaciones", pubFiltered);
-		
-		boolean esValidoPostulante = false;
-		Cookie[] cookies = req.getCookies();
-		String jwtCookieName = "jwt";
-		String jwt = null;
-		if (cookies != null) {
-			for (Cookie cookie : cookies) {
-				if (jwtCookieName.equals(cookie.getName())) {
-					jwt = cookie.getValue();
-					break;
-				}
-			}
-		}
-		if (jwt != null) {	
-			if (!portUsuarios.isTokenBlacklisted(jwt)) {
-				try {
-					
-					if (portUsuarios.validarToken(jwt)) {
-						String correo = portUsuarios.obtenerCorreoPorJWT(jwt);
-						DtUsuario usuario = portUsuarios.consultarUsuarioPorCorreo(correo); 
-						if (portUsuarios.tipoUsuario(jwt).equals("postulante")) {
-							esValidoPostulante = true;
-							String imagen = usuario.getUrlImagen();
-							req.setAttribute("imgPerfil", imagen);
-						}
-					} 
-
-				} catch (Exception e) {
-
-				}
-
-			}
-
-		} 
-		
-    	if(esValidoPostulante) {
-    		req.getRequestDispatcher("/WEB-INF/postulante/dashboardPostulante.jsp").forward(req, resp);
-    	}else {
-    		resp.sendRedirect("visitante");
-    	}
-    		
+    private boolean isMobileRequest(HttpServletRequest req) {
+        String userAgent = req.getHeader("User-Agent");
+        return userAgent != null && userAgent.matches(".*(Android|iPhone|iPad|iPod|Windows Phone|webOS|BlackBerry|Mobile).*");
     }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		try {
-			processRequest(request, response);
-		} catch (ServletException | IOException | KeywordExisteException_Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+    private String getJwtFromCookies(Cookie[] cookies) {
+        if (cookies == null) return null;
+        return Arrays.stream(cookies)
+                .filter(c -> JWT_COOKIE_NAME.equals(c.getName()))
+                .findFirst()
+                .map(Cookie::getValue)
+                .orElse(null);
+    }
 
+    private void dispatchToPage(HttpServletRequest req, HttpServletResponse resp, String page) throws ServletException, IOException {
+        req.getRequestDispatcher(page).forward(req, resp);
+    }
+
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, KeywordExisteException_Exception {
+        boolean isMobile = isMobileRequest(req);
+        req.setAttribute("keywords", portOfertas.obtenerKeywords().getItem());
+        List<DtPublicacion> publicaciones = getPublicaciones(req);
+        req.setAttribute("publicaciones", filtrarPublicacionesConfirmadas(publicaciones));
+
+        String jwt = getJwtFromCookies(req.getCookies());
+        if (jwt != null && portUsuarios.validarToken(jwt)) {
+            String tipoUsuario = portUsuarios.tipoUsuario(jwt);
+            if ("postulante".equals(tipoUsuario)) {
+                if (isMobile) {
+                    dispatchToPage(req, resp, "/WEB-INF/mobile/inicio.jsp");
+                } else {
+                    dispatchToPage(req, resp, "/WEB-INF/postulante/dashboardPostulante.jsp");
+                }
+            } else {
+                resp.sendRedirect("visitante");
+            }
+        } else {
+        	resp.sendRedirect("visitante");
+        }
+    }
+
+    private List<DtPublicacion> getPublicaciones(HttpServletRequest req) {
+        String busqueda = req.getParameter("busqueda");
+        String[] keywordsSeleccionadas = req.getParameterValues("keywords");
+        if (busqueda != null && !busqueda.isEmpty()) {
+            return portOfertas.obtenerPublicacionesPorBusqueda(busqueda).getItem();
+        } else if (keywordsSeleccionadas != null && keywordsSeleccionadas.length > 0) {
+            String keywordsString = String.join("/", keywordsSeleccionadas);
+            return portOfertas.obtenerPublicacionesPorKeywords(keywordsString).getItem();
+        }
+        return portOfertas.obtenerPublicaciones().getItem();
+    }
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ServletException | IOException | KeywordExisteException_Exception e) {
+            // Properly log the exception and handle it, if required
+            throw new ServletException(e);
+        }
+    }
+
+  
 }
+	
